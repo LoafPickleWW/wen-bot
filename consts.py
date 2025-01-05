72c7ee916a325c0f21476cc828f5ded99d6a4c9e
LoafPickle <loafpickle@protonmail.com>
2025-01-05 11:42:10 -0500

    Update consts.py
    Updated Search

--- consts.py
+++ consts.py
@@ -1,7 +1,7 @@
 HEADERS = {'accept': 'application/json', 'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'}
 
 NETWORK_ID = 0
-SEARCH_URL = "https://indexer.vestige.fi/assets/search?network_id=0&query={}"
+SEARCH_URL = "https://indexer.vestige.fi/assets/search?network_id=0&query={}&order_by=rank&order_dir=asc&limit=10"
 
 CANDLE_INTERVALS = 10000
 
